ec37d06dbd7463df31df61421a57763f40243cb3
MiiaKay <miiaknuuti@gmail.com>
2019-10-28 14:55:17 +0100

    solved exercise in sub chapter 1.2

---
+++ 1.1/1/main.c
@@ -0,0 +1,12 @@
+  #include<stdio.h>
+
+  int main(){
+    printf("Låt oss göra lite matematik:\n"); fflush(stdout);/*Fungerar utan att använda esc-sekv på min kompilator */
+    printf("Skriv ditt första tal: \n"); fflush(stdout);
+    float i, j;
+    scanf("%f",&i);
+    printf("Skriv ditt andra tal: \n"); fflush(stdout);
+    scanf("%f",&j);
+    printf("%.3f * %.3f = %.3f",i,j,(i*j));
+    return 0;
+  }
---
+++ 1.2/1/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  int i;
+  double j=0;
+  if (1==scanf("%d", &i))
+  {
+    printf("Ett tal!");
+    i *=i;
+    printf("%d \n", i);
+    while(pow(10,++j)<i);
+    {;}
+    printf("%.0f",j);
+  }
+  else
+  {
+    printf("Inte ett tal!");
+  }
+
+  return 0;
+}
